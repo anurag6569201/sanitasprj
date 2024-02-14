a27e543e2fefa9a1954b8b4efb0b9f7ea93edbdc
anurag singh <anurag6569201@gmail.com>
2024-02-14 13:20:06 +0530

    adjusted settings for render

--- sanitasprj/settings.py
+++ sanitasprj/settings.py
@@ -139,6 +139,10 @@ STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
+# renderstorage
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# ......
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
